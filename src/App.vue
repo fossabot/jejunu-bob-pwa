b9f9e76b76ef4dff91b9dc18afa34e580265d88c
Muhun Kim <iam@muhun.kim>
2019-07-14 00:02:45 +0900

    Add weekends to 'weekDict' variable

--- src/App.vue
+++ src/App.vue
@@ -49,14 +49,16 @@ export default {
     data: () => ({
         isWeekday: true,
         weekDict: {
+			0: '일요일',
             1: '월요일',
             2: '화요일',
             3: '수요일',
             4: '목요일',
-            5: '금요일'
+			5: '금요일',
+			6: '토요일'
         },
         week: '월요일',
-        time: '점심',
+        time: '',
         timeKey: { 점심: [true, false], 저녁: [false, true] },
         panel: [true, false],
         total: {},
