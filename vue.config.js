b7c3ff655d0ae48f5cbf0c939177c8cd2734de93
Muhun Kim <iam@muhun.kim>
2019-07-13 23:55:44 +0900

    add backgroud color in manifest

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
         workboxOptions: {
             swSrc: 'public/service-worker.js'
         },
-        themeColor: '#F1F1F1'
+		themeColor: '#F1F1F1',
+		background_color: '#F1F1F1'
     }
 }
