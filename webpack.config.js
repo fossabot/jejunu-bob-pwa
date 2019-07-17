11ac9c280da28287aaebeab603ab32b64f385b8f
Muhun Kim <iam@muhun.kim>
2019-07-17 23:28:11 +0900

    Use CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const mode = process.env.prod ? 'production' : 'development'
 
@@ -31,6 +32,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'public/index.html'
         })
