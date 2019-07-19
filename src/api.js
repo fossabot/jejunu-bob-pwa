934a5e7a1261e760f457c5fbd2f81ba38e95857e
Muhun Kim <iam@muhun.kim>
2019-07-19 18:35:07 +0900

    use object destructure in axios import

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
-import axios from 'axios'
+import { get } from 'axios'
 
 const DOMAIN = 'https://meals-data.muhun.kim'
 
 export const request = () => {
-	return axios.get(DOMAIN).then(res => res.data)
-		.catch(res => {
-			throw res
-		})
+    return get(DOMAIN)
+        .then(res => res.data)
+        .catch(res => {
+            throw res
+        })
 }
